ed75878cfaa555aeb7bbc12d5a6420c75f57aa9f
Julio L <lamajulio17@gmail.com>
2018-11-18 10:04:45 -0600

    Adding Clarity Design

--- newsApplicationF/src/app/app.module.ts
+++ newsApplicationF/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+/* Routing */
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Routes
+const routes :  Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
